3b40e5dda0476e9cabeed9c2c3729da9ce2db910
Bonivárt Krisztián <krisztian.bonivart@gmail.com>
2023-06-14 04:45:58 +0200

    Szorgalmi feladat

--- src/Month.java
+++ src/Month.java
@@ -1,2 +1,50 @@
-package PACKAGE_NAME;public enum Month {
+import java.util.Random;
+
+public enum Month {
+
+    JANUARY("Január"),
+    FEBRUARY("Február"),
+    MARCH("Március"),
+    APRIL("Április"),
+    MAY("Május"),
+    JUNE("Június"),
+    JULY("Július"),
+    AUGUST("Augusztus"),
+    SEPTEMBER("Szeptember"),
+    OCTOBER("Október"),
+    NOVEMBER("November"),
+    DECEMBER("December");
+
+    public String hungarianName;
+
+    private Month (String hungarianName){
+        this.hungarianName=hungarianName;
+    }
+
+    public String getHungarianName() {
+        return hungarianName;
+    }
+    public boolean summerMonth(){
+        return this == JUNE || this == JULY || this == AUGUST;
+    }
+    public boolean autumnMonth(){
+        return this == SEPTEMBER || this == OCTOBER || this ==NOVEMBER;
+    }
+    public boolean winterMonth (){
+        return this == DECEMBER || this == JANUARY || this == FEBRUARY;
+    }
+    public boolean springMonth (){
+        return this == MARCH || this == APRIL || this == MAY;
+    }
+
+    public int getMonthNumber(){
+        return this.ordinal()+1;
+    }
+    public static Month randomMonth(){
+        Random random = new Random();
+        Month[] months= Month.values();
+        return months[random.nextInt(months.length)];
+    }
 }
+
+
--- src/Season.java
+++ src/Season.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public enum Season {
+public enum Season {
+
+    SPRING,
+    SUMMER,
+    AUTUMN,
+    WINTER;
+
+    public Season getSeasonByMonth ( Month month){
+        return switch (month) {
+            case MARCH, APRIL, MAY -> SPRING;
+            case JUNE, JULY, AUGUST -> SUMMER;
+            case SEPTEMBER, OCTOBER, NOVEMBER -> AUTUMN;
+            case DECEMBER, JANUARY, FEBRUARY -> WINTER;
+        };
+    }
+    public String getHungarianName (){
+        return switch (this){
+            case SPRING -> "tavasz";
+            case SUMMER -> "Nyár";
+            case AUTUMN -> "Ősz";
+            case WINTER -> "Tél";
+        };
+    }
 }
